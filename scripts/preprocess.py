d83ba08fcc770be0c3d99cbf497e4a532d4e7075
Bouizdouzene Bilal <189028699+BilalBouizdouzene@users.noreply.github.com>
2025-05-16 14:04:18 +0200

    Description de la modification

---
+++ scripts/preprocess.py
@@ -0,0 +1,13 @@
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+dataset = ImageFolder(root="data/raw/classification", transform=transform)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+print(dataset.classes)  # ['cloudy', 'desert', 'green_area', 'water']
